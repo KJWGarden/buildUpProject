05f82289b3ec5320cef6f5440c14789961460dfe
KJWGarden <mike03131313@gmail.com>
2024-12-02 18:55:25 +0900

    feat: Carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import Content from "@/components/content";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 export default function Home() {
   return (
-    <main className="w-full h-full flex">
-      <Header />
+    <main className="w-full h-screen flex">
+      <div className="fixed top-0 w-full">
+        <Header />
+      </div>
+      <div className="flex-grow flex items-center justify-center">
+        <Content />
+      </div>
       <div className="fixed bottom-0 w-full">
         <Footer />
       </div>
