f33d6f4319978d956c01dcfd7049b1b97fc307ef
KJWGarden <mike03131313@gmail.com>
2024-12-05 16:53:43 +0900

    feat: login Form 추가 login페이지 Form 추가 및 zod validation 추가

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,83 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
+const formSchema = z.object({
+  id: z.string().min(2, {
+    message: "부적절한 아이디입니다. 다시 입력해주세요",
+  }),
+  password: z.string().min(2, {
+    message: "부적절한 비밀번호입니다. 다시 입력해주세요",
+  }),
+});
+
 export default function Login() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <div className="w-full flex-col justify-items-center">
       <div className="text-4xl font-extrabold pt-32">BIBLE UP</div>
-      <div className="w-1/4 pt-10 flex-col space-y-4">
-        <Input type="id" placeholder="Id" />
-        <Input type="password" placeholder="password" />
-      </div>
-      <div className="pt-10">
-        <Button>Login</Button>
+      <div className="pt-5">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 flex-col justify-items-center "
+          >
+            <FormField
+              control={form.control}
+              name="id"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>아이디</FormLabel>
+                  <FormControl>
+                    <Input placeholder="id" {...field} />
+                  </FormControl>
+                  <FormDescription>아이디를 입력하세요.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>비밀번호</FormLabel>
+                  <FormControl>
+                    <Input placeholder="password" {...field} />
+                  </FormControl>
+                  <FormDescription>비밀번호를 입력하세요.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
       </div>
     </div>
   );
